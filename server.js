19bbaf08536072f30f585159e9fce8ba34c62308
WHAT A LIFE <lagaritclawiji22@gmail.com>
2026-07-14 02:26:59 +0800

    Use Render PORT environment variable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // ===============================
 // DATABASE
